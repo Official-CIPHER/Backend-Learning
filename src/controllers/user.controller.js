819a620c06280d6ea0b86baa8401ff11cf840927
Official-CIPHER <vishalkumarsharma911@gmail.com>
2024-09-16 11:35:12 +0530

    api response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -98,7 +98,7 @@ const registerUser = asyncHandler(async(req,res) =>{
 
 
     // return response
-    return res.status(201).json(new ApiError(200, createdUser, "User registered Successfully"))
+    return res.status(201).json(new ApiResponse(200, createdUser, "User registered Successfully"))
 
 })
 
