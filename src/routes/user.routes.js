95bfc0029956d304365c85e1b1c8b5c9f0d548b3
Official-CIPHER <vishalkumarsharma911@gmail.com>
2024-09-22 09:42:05 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { logoutUser,loginUser,registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    logoutUser,
+    loginUser,
+    registerUser,
+    refreshAccessToken, changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -32,6 +40,33 @@ router.route("/login").post(loginUser)
 // secure routes
 // Logout route with post request and one verifyJWT middleware
 router.route("/logout").post(verifyJWT , logoutUser)
+
+// this route is use to get the refresh token 
 router.route("/refresh-token").post(refreshAccessToken)
+
+// Change password route
+router.route("/change-password").post(verifyJWT,
+    changeCurrentPassword)
+
+//  Use to get current user
+router.route("/cuurent-user").get(verifyJWT,getCurrentUser)
+
+// update account details router
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+// update the avatar routes
+
+// upload.single - use to upload single file
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+// update cover Image
+router.route("/coverImage").patch(verifyJWT,upload.single("/coverImage"), updateUserCoverImage)
+
+// Get channel profile
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+// Wacth History route
+router.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default router
